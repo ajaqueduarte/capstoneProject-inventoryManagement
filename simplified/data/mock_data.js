97571e550a317c4cc41546383ed0770629a8df93
valunchanut <125695721+valunchanut@users.noreply.github.com>
2024-04-01 12:18:15 -0400

    Added simplified web interface
    Added simplified web interface

---
+++ simplified/data/mock_data.js
@@ -0,0 +1,11 @@
+// mock_data.js
+
+const mockData = [
+    { date: '2023-12-28', nuttinBetter: 5.0, bananaAWhey: 12.0, rippedBerry: 11.0, strawberryStorm: 9.0, highImpactAcai: 6.0, acaiAvalanche: 3.0, mindOverMatcha: 7.0 },
+    { date: '2023-12-29', nuttinBetter: 17.0, bananaAWhey: 10.0, rippedBerry: 7.0, strawberryStorm: 6.0, highImpactAcai: 13.0, acaiAvalanche: 9.0, mindOverMatcha: 13.0 },
+    { date: '2023-12-30', nuttinBetter: 8.0, bananaAWhey: 9.0, rippedBerry: 8.0, strawberryStorm: 12.0, highImpactAcai: 9.0, acaiAvalanche: 4.0, mindOverMatcha: 6.0 },
+    // ... more data objects
+  ];
+  
+  export default mockData;
+  
